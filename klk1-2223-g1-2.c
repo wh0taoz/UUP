013183ebd38e8747f9e162dd3cb0a6e2a2c0aca2
simona <104918986+wh0taoz@users.noreply.github.com>
2024-11-22 14:11:37 +0100

    Add files via upload

--- klk1-2223-g1-2.c
+++ klk1-2223-g1-2.c
@@ -16,31 +16,35 @@ int main(){
                 	c2/=10;
                 	k--;
                 	i++;
+
             	}
             	printf("%d", c3);
             	i = 1;
             	c3 =0;
             	c2 = 0;
+            	dev = 1;
         	}
         	else if (prethodni != trenutni && k>0){
+                int kv = k;
             	while (k>0){
                 	c3 = (c2%10)*(int)pow(10, (k-1)) + c3;
                 	c2/=10;
                 	k--;
             	}
-            	c3 = c3%(k*k);
+            	c3 = c3%(kv*kv);
             	printf("%d", c3);
             	i = 1;
             	c3 =0;
             	c2 = 0;
+            	dev = 1;
         	}
 
         	putchar(c1);
     	}
-    	else if (c1>= '0' || c1<= '9'){
+    	else if (c1>= '0' && c1<= '9'){
         	k++;
         	c2 = c2 + (c1 - '0') * dev;
-
+        	dev = dev*10;
 
 
     	}
